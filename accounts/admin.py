6ee25b16aa04d79c9f1bac9b1241495f0a8e8c58
hbuiOnline <61067968+hbuiOnline@users.noreply.github.com>
2021-02-28 18:39:00 -0600

    Database Models & Admin Panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import  * # To import all the model from .models, then specify those in register
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+# Create class that represent our database
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True) #null=true will let insert into table without filling out all the fields
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True) #Auto create/insert time whenever there is created instance in the Customer table
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = ( #tuple
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+
+    STATUS = ( #tuple
+        ('Pending', 'Pending'),
+        ('Out of Delivery', 'Out of Delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+    # customer = 
+    # product = 
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
