e8535c05d3955ff681af08f5b3b565c71c96ac01
hbuiOnline <61067968+hbuiOnline@users.noreply.github.com>
2021-04-01 20:33:20 -0500

    Password Reset email with customer templates

--- CRM/settings.py
+++ CRM/settings.py
@@ -136,3 +136,12 @@ STATICFILES_DIRS = [  # This is for linking the static folder into django app fo
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_ROOT = BASE_DIR / "static/images"
+
+
+# SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bugtracker2020@gmail.com'
+EMAIL_HOST_PASSWORD = 'Hviponlin3'
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -7,7 +10,6 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
-
 
 
     # using name to reference for dynamic routing
@@ -23,5 +25,32 @@ urlpatterns = [
     path('update_order/<str:pk>', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>', views.deleteOrder, name="delete_order"),
 
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(
+             template_name="accounts/password_reset.html"),
+         name="reset_password"),
 
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="accounts/password_reset_sent.html"),
+         name="password_reset_done"),
+
+    # uidb64: The user's id encoded in base 64
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="accounts/password_reset_form.html"),
+         name="password_reset_confirm"),
+    # token: Token to check that the password is valid
+
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="accounts/password_reset_done.html"),
+         name="password_reset_complete"),
+
+
+
+    # 1 - Submit Email Form                       //PasswordResetView.as_view()
+    # 2 - Email sent success message              //PasswordResetDoneView.as_view()
+    # 3 - Link to password Reset form in email    //PasswordResetConfirmView.as_view()
+    # 4 - Password successfully changed message   //PasswordResetCompleteView.as_view()
 ]
